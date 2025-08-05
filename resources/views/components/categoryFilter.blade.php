b249500e9fcd6cacbd30770232c43247f94b2695
Geru <113689254+Gelatino0308@users.noreply.github.com>
2025-08-05 13:31:55 +0800

    refactor: restructured post card layout

--- resources/views/components/categoryFilter.blade.php
+++ resources/views/components/categoryFilter.blade.php
@@ -1,7 +1,7 @@
 @props(['categories' => []])
 
 <div class="mb-4 text-lg">
-    <label for="category" class="text-lg">Filter by Category:</label>
+    <label for="category" class="text-lg">Filter by Genre:</label>
     <select name="category" id="category" 
             class="p-1 rounded-md ring-1 ring-slate-600 focus:ring-slate-900 focus:ring-2 outline-0 border-0 mt-1" 
             x-model="selectedCategory">
